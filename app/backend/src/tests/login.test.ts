ed91aea6978a461e3a65a1f38b5e892aa90ca3ff
celsorodrigo <celsorodrigoma@gmail.com>
2022-11-23 17:24:37 -0300

    Refatorados testes de autenticacao de token para serem feitos nos testes da rota /login.

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -7,6 +7,7 @@ import chaiHttp = require('chai-http');
 import App from '../app';
 
 import { Response } from 'superagent';
+import JwtToken from '../middlewares/JwtToken';
 
 chai.use(chaiHttp);
 
@@ -53,13 +54,27 @@ describe('Tests of /login', () => {
   });
 
   describe('GET /login/validate', () => {
-    it('shouldn return the correct role and status 200', async () => {
+    it('should return the correct role and status 200', async () => {
       const body = { email: "admin@admin.com", password: "secret_admin" };
       const login = await chai.request(app).post('/login').send(body);
-      const response = await chai.request(app).get('/login/validate').set('authorization', login.body.token)
+      const response = await chai.request(app).get('/login/validate')
+        .set('authorization', login.body.token)
       expect(response.status).to.be.equal(200);
       expect(response.body.role).to.be.equal('admin');
-    })
+    });
+
+    it('shouldn\'t be able to login with out a token', async () => {
+      const response = await chai.request(app).get('/login/validate');
+      expect(response.status).to.be.equal(401);
+      expect(response.body.message).to.be.equal('Token not found');
+    });
+
+    it('shouldn\'t be able to login with a invalid token', async () => {
+      const response = await chai.request(app).get('/login/validate')
+        .set('authorization', 'InvalidToken');
+      expect(response.status).to.be.equal(401);
+      expect(response.body.message).to.be.equal('Invalid token');
+    });
   });
 
 });
