458bb66a88bdd464581d46ce43ec4831ae4da038
celsorodrigo <celsorodrigoma@gmail.com>
2022-11-23 15:12:28 -0300

    Adicionados novos testes de POST /login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,3 +1,4 @@
+import * as res from './mocks/responsesMocks';
 import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
@@ -15,11 +16,39 @@ const { expect } = chai;
 
 describe('Tests of /login', () => {
   describe('POST /login', () => {
-    it('should be able to login and return a token', async () => {
+    it('should be possible to login and return a token', async () => {
       const body = { email: "admin@admin.com", password: "secret_admin" };
       const response = await chai.request(app).post('/login').send(body);
       expect(response.status).to.be.equal(200);
       expect(response.body).to.have.property('token');
-    })
+    });
+
+    it('shouldn\'t be possible to login with an empty email', async () => {
+      const body = { email: "", password: "secret_admin" };
+      const response = await chai.request(app).post('/login').send(body);
+      expect(response.status).to.be.equal(400);
+      expect(response.body.message).to.be.equal(res.EMPTY_FIELDS);
+    });
+
+    it('shouldn\'t be possible to login with an empty password', async () => {
+      const body = { email: "admin@admin.com", password: "" };
+      const response = await chai.request(app).post('/login').send(body);
+      expect(response.status).to.be.equal(400);
+      expect(response.body.message).to.be.equal(res.EMPTY_FIELDS);
+    });
+
+    it('shouldn\'t be possible to login with a invalid email', async () => {
+      const body = { email: "invalid", password: "secret_admin" };
+      const response = await chai.request(app).post('/login').send(body);
+      expect(response.status).to.be.equal(400);
+      expect(response.body.message).to.be.equal(res.INVALID_FIELDS);
+    });
+
+    it('shouldn\'t be possible to login with a invalid password', async () => {
+      const body = { email: "admin@admin.com", password: "short" };
+      const response = await chai.request(app).post('/login').send(body);
+      expect(response.status).to.be.equal(400);
+      expect(response.body.message).to.be.equal(res.INVALID_FIELDS);
+    });
   });
 });
